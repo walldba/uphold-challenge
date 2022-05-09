8a93d643f0035c59d34a9a39088dfc7224fbf351
wallace.jeronimo <wallace.jeronimo@bitcapital.com.br>
2022-05-09 20:06:07 -0300

    feat(uphold): add uphold service

---
+++ src/uphold/services/uphold.service.ts
@@ -0,0 +1,21 @@
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AxiosResponse } from 'axios';
+import { firstValueFrom } from 'rxjs';
+import { UpholdConfig } from '../../config/uphold.config';
+import { ICurrencyResponse } from '../interfaces/uphold.currency.response';
+import { IUpholdService } from '../interfaces/uphold.service.interface';
+
+@Injectable()
+export class UpholdService implements IUpholdService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async getTicketCurrency(
+    currencyPair: string,
+  ): Promise<AxiosResponse<ICurrencyResponse, any>> {
+    return await firstValueFrom(
+      this.httpService.get<ICurrencyResponse>(`/ticker/${currencyPair}`),
+    );
+  }
+}
